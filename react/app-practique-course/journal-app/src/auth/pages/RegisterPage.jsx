8adf7e7f04aeb49000705c46b09647b548e79517
samirjabib <samir.jc123@gmail.com>
2022-11-15 21:41:18 -0500

    add new project

---
+++ react/app-practique-course/journal-app/src/auth/pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+
+export const RegisterPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
---
+++ react/app-practique-course/journal-app/src/journal/pages/JournalPage.jsx
@@ -0,0 +1,5 @@
+export const JournalPage = () => {
+    return(
+        <div>Journal Page</div>
+    )
+}
